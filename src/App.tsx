251b703ae8fd5236c5b234d821c34a13dcab8dc1
Dean Hardaker <ober9k@users.noreply.github.com>
2026-03-15 00:28:51 +1100

    Added basic key press tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 export default function App() {
+
+  useEffect(() => {
+    const keydownHandler = (e: KeyboardEvent) => {
+      console.log('keydown', e.code, e.key);
+    };
+
+    document.addEventListener('keydown', keydownHandler)
+
+    return () => {
+      document.removeEventListener('keydown', keydownHandler)
+    };
+  }, [])
+
   return (
     <div>
       Wordle Clone
